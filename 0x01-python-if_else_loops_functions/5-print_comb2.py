530418ec264cfd9db629486938819917e7b10104
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-11-29 23:57:56 +0100

    Combination 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for item in range(0, 100):
     if item != 99:
-        print(f"{item:02}", end=", ")
+        print("{:02}".format(item), end=", ")
     else:
-        print(f"{item}")
+        print("{}".format(item))
