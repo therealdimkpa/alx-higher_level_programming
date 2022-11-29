47acd9aaaf3c189fb6d8257a42c456a99a359311
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-11-30 00:25:00 +0100

    Uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,5 @@ def uppercase(str):
     for char in str:
         if ord(char) >= ord('a') and ord(char) <= ord('z'):
             char = chr(ord(char) - 32)
-            print("{}".format(char), end="")
-        else:
-            print("{}".format(char), end="")
+        print("{}".format(char), end="")
     print()
