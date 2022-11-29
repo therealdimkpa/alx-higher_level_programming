852610e7ebb893fa9107093f75535dd48d8c727d
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-11-30 00:12:47 +0100

    isLower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+
 def islower(c):
-    for i in range(ord('a'), ord('z') +1):
+    for i in range(ord('a'), ord('z') + 1):
         if chr(i) == c:
             return True
     return False
