1f6209e7a760141ff4b7f6464ffcb279994a3498
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-11-29 22:05:37 +0100

    Print alphabet without e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for item in range(ord('a'), ord('z') + 1):
-    if item == ord('e') or item == ord('z'):
+    if chr(item) == ord('e') or chr(item) == ord('z'):
         continue
     else:
         print("{:c}".format(item), end="")
