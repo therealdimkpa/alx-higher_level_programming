4e46a01754e4dcec4c05a0a758ee128d3b6caf6e
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-11-30 00:51:05 +0100

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3i
 
 def fizzbuzz():
-    print("1", end="")
-    for i in range(2, 101):
+    for i in range(1, 101):
         if i%3 == 0 and i%5 == 0:
-            print(" FizzBuzz", end="")
+            print("FizzBuzz", end=" ")
         elif i%3 == 0:
-            print(" Fizz", end="")
+            print("Fizz", end=" ")
         elif i%5 == 0:
-            print(" Buzz", end="")
+            print("Buzz", end=" ")
         else:
-            print(" {}".format(i), end="")
+            print("{}".format(i), end=" ")
