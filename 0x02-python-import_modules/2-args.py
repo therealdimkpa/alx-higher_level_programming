50daad16e2fe5ec4135ccafa35292ba500a1177f
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-12-02 01:53:00 +0100

    Fixed code for zero args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc == 1:
         print("1 argument.")
+    elif argc = 0:
+        print("0 arguments.")
     else:
         print("{} arguments:".format(argc))
     for i in range(argc):
