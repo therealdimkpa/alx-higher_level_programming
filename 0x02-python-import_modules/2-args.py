06831b00d52e01255aef45f1887fb8d2a56e9576
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-12-02 01:51:11 +0100

    args with permission

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,8 @@ if __name__ == "__main__":
     import sys
 
     argc = len(sys.argv) - 1
-    if argc == 0:
-        print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:")
+    if argc == 1:
+        print("1 argument.")
     else:
         print("{} arguments:".format(argc))
     for i in range(argc):
