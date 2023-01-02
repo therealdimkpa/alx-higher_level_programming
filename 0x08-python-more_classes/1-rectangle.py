8f23405f722c4af89397e3437bb414d70b3261a8
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2023-01-03 00:24:32 +0100

    Fixed last commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,19 +19,8 @@ class Rectangle:
                 height (int): height of the new rectangle
         """
 
-        if (type(height) != int):
-            raise TypeError("height must be integer")
-        elif (height < 0):
-            raise ValueError("height must be >= 0")
-        else:
-            self.height = height
-
-        if (type(width) != int):
-            raise TypeError("width must be integer")
-        elif (width < 0):
-            raise ValueError("width must be >= 0")
-        else:
-            self.width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
