406c36f1ee8fa3dbb19fd6935aee8409aa4a738c
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2023-01-02 11:12:50 +0100

    Adds property getter and setter

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+
+        if(type(height) != int):
+            raise TypeError("height must be integer")
+        elif (height < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+        if (type(width) != int):
+            raise TypeError("width must be integer")
+        elif (width < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
