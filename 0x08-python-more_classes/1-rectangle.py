bb335368e80a4d30a2cc3bc6743c5f79a7aaae69
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2023-01-02 11:20:43 +0100

    Added conditional parameters for modifying height and width

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,12 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        self.__width = value
+        if (type(value) != int):
+            raise TypeError("width must be integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -31,4 +36,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        self.__height = value
+        if(type(value) != int):
+            raise TypeError("height must be integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
