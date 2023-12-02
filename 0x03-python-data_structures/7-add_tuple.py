d5d2bc69059c484d91d42bbb640896ed113fb936
Dimkpa Emmanuel <dimkpaemmanuel1@gmail.com>
2023-12-02 10:48:21 +0100

    Add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    adds the first elements of two tuples
+    and the second elements of two tuples
+    """
 
+    final_tuple = [0, 0]
+    if len(tuple_a) > 0:
+        final_tuple[0] += tuple_a[0]
+    if len(tuple_b) > 0:
+        final_tuple[0] += tuple_b[0]
+
+    if len(tuple_a) > 1:
+        final_tuple[1] += tuple_a[1]
+    if len(tuple_b) > 1:
+        final_tuple[1] += tuple_b[1]
+
+    return(tuple(final_tuple))
