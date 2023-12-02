b675353f2763acc0203d7e7b44fe2549c3a01bf8
Dimkpa Emmanuel <dimkpaemmanuel1@gmail.com>
2023-12-02 11:13:43 +0100

    Doctype + Return None

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
+    """
+    Args:
+        my_list (list, optional): Defaults to [].
+
+    Returns:
+        int: The maximum number in the list
+    """
+    
     if (len(my_list) == 0):
-        return 0
+        return None
     else:
         my_list.sort()
         return (my_list[len(my_list) - 1])
