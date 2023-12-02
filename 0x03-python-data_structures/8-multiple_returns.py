11a8af357126726faeea85cb6a0732ad382c5158
Dimkpa Emmanuel <dimkpaemmanuel1@gmail.com>
2023-12-02 11:05:44 +0100

    Added doctype

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
+    """
+    multiple_returns
+
+    Args:
+        sentence (string): Any type of sentence or string
+
+    Returns:
+        tuple(length of input, first char of input)
+    """
     if len(sentence) == 0:
         return (len(sentence), None)
     else:
