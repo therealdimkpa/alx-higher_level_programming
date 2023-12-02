60c44ed0d0d1bb7236faeca908320ad33a1bdd7b
Dimkpa Emmanuel <dimkpaemmanuel1@gmail.com>
2023-12-02 11:29:01 +0100

    What?

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include Python.h
+#include <stdio.h>
+
+void print_python_list_info(PyObject *p)
+{
+	int  i;
+	PyObject *obj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
