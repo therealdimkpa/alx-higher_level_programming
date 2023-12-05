c1313c84ece2cee34f3bf3bd9d584f65cf554323
Dimkpa Emmanuel <dimkpaemmanuel1@gmail.com>
2023-12-05 11:13:04 +0100

    Pycodestyle compliance

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,10 +7,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """
 
     final_tuple = [0, 0]
-    if len(tuple_a) > 0: final_tuple[0] += tuple_a[0]
-    if len(tuple_b) > 0: final_tuple[0] += tuple_b[0]
+    if len(tuple_a) > 0:
+        final_tuple[0] += tuple_a[0]
+    if len(tuple_b) > 0:
+        final_tuple[0] += tuple_b[0]
 
-    if len(tuple_a) > 1: final_tuple[1] += tuple_a[1]
-    if len(tuple_b) > 1: final_tuple[1] += tuple_b[1]
+    if len(tuple_a) > 1:
+        final_tuple[1] += tuple_a[1]
+    if len(tuple_b) > 1:
+        final_tuple[1] += tuple_b[1]
 
     return (tuple(final_tuple))
+
