4cfbac20d43b2ff16e25ff44fdc970583336d8c3
Dimkpa Emmanuel <dimkpaemmanuel1@gmail.com>
2023-12-05 11:11:10 +0100

    Added check for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    end = len(my_list) - 1
-    while (end >= 0):
-        print("{:d}".format(my_list[end]))
-        end -= 1
+    if my_list != None:
+        end = len(my_list) - 1
+        while (end >= 0):
+            print("{:d}".format(my_list[end]))
+            end -= 1
