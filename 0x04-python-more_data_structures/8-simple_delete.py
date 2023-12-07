05666c0e67e21724ac27d585f7b5c6aaa1ed67e3
Dimkpa Emmanuel <dimkpaemmanuel1@gmail.com>
2023-12-07 08:08:27 +0100

    Pycodestyle

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
+    """simple_delete
+
+    Args:
+        a_dictionary (_type_): _description_
+        key (str, optional): _description_. Defaults to "".
+
+    Returns:
+        _type_: _description_
+    """
     if key in a_dictionary:
-        del(a_dictionary[key])
+        del (a_dictionary[key])
     return (a_dictionary)
